be612f60eba13b6de22a5e1bc631792e184f9b07
inos2023 <inos2023@163.com>
2023-11-20 15:24:35 +0800

    首页主题图片

--- src/components/HomePage/HomepageFeatures/index.js
+++ src/components/HomePage/HomepageFeatures/index.js
@@ -1,40 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import png1 from './01-3算力多样化.png';
+import png2 from './01-4计算智能化.png';
+import png3 from './01-5定制模块化.png';
+import png4 from './01-6并行计算模型.png';
 
 const FeatureList = [
-
   {
     title: '算力多样化',
-    Svg: require('@site/static/img/01-3算力多样化.svg').default,
-    description: (
-      <>
-        支持主流的CPU+xPU异构算力
-      </>
-    ),
+    pic: png1,
+    // Svg: require('@site/static/img/01-3算力多样化.svg').default,
+    description: <>支持主流的CPU+xPU异构算力</>,
   },
   {
     title: '计算智能化',
-    Svg: require('@site/static/img/01-4计算智能化.svg').default,
+    pic: png2,
+    // Svg: require('@site/static/img/01-4计算智能化.svg').default,
     description: (
       <>
         内置轻量级AI框架
-        <p>支持边/端智能计算</p>        
+        <p>支持边/端智能计算</p>
       </>
     ),
   },
   {
     title: '定制模块化',
-    Svg: require('@site/static/img/01-5定制模块化.svg').default,
-    description: (
-      <>
-        支持按需定制模块
-      </>
-    ),
+    pic: png3,
+    // Svg: require('@site/static/img/01-5定制模块化.svg').default,
+    description: <>支持按需定制模块</>,
   },
   {
     title: '并行计算模型',
-    Svg: require('@site/static/img/01-6并行计算模型.svg').default,
+    pic: png4,
+    // Svg: require('@site/static/img/01-6并行计算模型.svg').default,
     description: (
       <>
         构建并行计算模型
@@ -44,11 +43,12 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ pic, title, description }) {
   return (
     <div className={clsx('col col--3')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+        <img src={pic} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
